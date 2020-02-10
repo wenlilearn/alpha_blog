dcb38da9d05ed1d1a7f55d2fd235393871cf9eb0
wenlilearn <wenlilearn@gmail.com>
2020-02-10 00:35:50 -0500

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-  
+  validates :username, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: { case_sensitive: false }
+  VAILD_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+/i
+  validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, format: { with: VAILD_EMAIL_REGEX }
 end
