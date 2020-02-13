2f346bd83e46c49ec33002c8bf4acda42dc434b6
wenlilearn <wenlilearn@gmail.com>
2020-02-13 01:20:34 -0500

    Edit categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :require_admin, only: [:new, :create]
+  before_action :require_admin, only: [:new, :create, :edit, :update]
 
   def index
     @categories = Category.paginate(page: params[:page], per_page: 3)
@@ -16,6 +16,20 @@ class CategoriesController < ApplicationController
       redirect_to categories_path
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category was successfully updated"
+      redirect_to category_path(@category) 
+    else
+      render 'edit'
     end
   end
 
