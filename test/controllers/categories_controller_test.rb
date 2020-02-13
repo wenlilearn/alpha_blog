56ae5dc018ea787e9638afac9d98d1227f2ba379
wenlilearn <wenlilearn@gmail.com>
2020-02-13 00:27:07 -0500

    Make tests all green and fix nav bar

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @category = Category.create(name: "sports")
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
   end
 
   test "should get categories index" do
@@ -11,12 +12,20 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    sign_in_as(@user, "passwords")
     get new_category_path
-    assert_response :success
+    assert_redirected_to categories_path
   end
 
   test "should get show" do
     get category_path(@category)
     assert_response :success
+  end
+
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: {name: "sports"} }
+    end
+    assert_redirected_to categories_path
   end
 end
