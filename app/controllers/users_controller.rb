3a5d824532784551043c88726e4acbfe58f9890a
Wen Li <wenlilearn@gmail.com>
2020-02-15 11:45:02 -0500

    Add admin user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 1)
@@ -37,6 +38,13 @@ class UsersController < ApplicationController
     @articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all articles have been deleted"
+    redirect_to users_path
+  end
+
   private
 
   def user_params
@@ -48,8 +56,15 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if logged_in? && current_user != @user
+    if logged_in? && current_user != @user && !current_user.admin
       flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+    end
+  end
+
+  def require_admin
+    if !logged_in? || !current_user.admin?
+      flash[:danger] = "Only admin users can perform that action"
       redirect_to root_path
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :articles
+  has_many :articles, dependent: :destroy
   before_save { self.email = email.downcase }
   has_secure_password
 
